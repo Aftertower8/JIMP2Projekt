517c5b12d5bef28013de7d4c9c8b91446b080a93
343306 <01206747@pw.edu.pl>
2026-03-02 21:11:36 +0100

    Add main function for graph file processing
    Implement main function to handle command line arguments for graph processing.

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc, char** argv)
+{
+  if(argc == 1){ //Sprawdzanie ilosci argumentow
+      fprintf(stderr,"Nie podano zadnych argumentow.\nProgram wymaga podania przynajmniej sciezki do pliku z grafem.\n");
+      return 1;
+  }
+
+  wczyt_graf(argv[1]); //wczytanie pliku z grafem
+  char a[50] = "triangulacja"; //Bazowo bez podania flagi argumentu bedzie triangulacja
+  FILE* out = NULL;
+  for (int j = 2; j < argc; j++) { //wczytywanie z flagami
+    if (strcmp(argv[j], "-v") == 0) { //tryb verbose
+        verbose(); //<-do zrobienia
+    }
+    if (strcmp(argv[j], "-h") == 0) { //tryb help
+        help(); //<-do zrobienia
+    }
+    if (strcmp(argv[j], "-a") == 0) { //zmiana na inny algorytm
+        j++;
+        if (j < argc)
+            if(sscanf(argv[j], "%49s", a) != 1) {
+              fprintf(stderr,"Bledny argument dla -a\n");
+              return 1;
+            }
+    }
+    if (strcmp(argv[j], "-w") == 0) { //otworzenie pliku z wynikiem
+        j++;
+        if (j < argc){
+            out = fopen(argv[j], "a");
+          if (!out) {
+            fprintf(stderr, "Nie mozna otworzyc pliku");
+            return 1;
+          }
+        }
+    }
+    if (strcmp(argv[j], "-w") != 0 &&
+        strcmp(argv[j], "-a") != 0 &&
+        strcmp(argv[j], "-h") != 0 &&
+        strcmp(argv[j], "-v") != 0) {
+        fprintf(stderr,"Nieznana flaga: %s\n", argv[j]);
+        return 1;
+    }
+  }
+
+  //wlaczanie funkcji posrednich
+  //do zrobienia
+  //zapisywanie wyniku
+  //do zrobienia
+  
+  if(out) fclose(out);
+  return 0;
+}
