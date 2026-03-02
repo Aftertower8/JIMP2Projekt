7e8a2cbd6159b323b8a98b63d64cb249f3dae706
343306 <01206747@pw.edu.pl>
2026-03-02 21:17:03 +0100

    Update pob_dane.c

--- pob_dane.c
+++ pob_dane.c
@@ -1,1 +1,34 @@
 
+  for (int j = 2; j < argc; j++) { //wczytywanie z flagami
+    if (strcmp(argv[j], "-v") == 0) { //tryb verbose
+        verbose(); //<-do zrobienia
+    }
+    if (strcmp(argv[j], "-h") == 0) { //tryb help
+        help(); //<-do zrobienia
+    }
+    if (strcmp(argv[j], "-a") == 0) { //zmiana na inny algorytm
+        j++;
+        if (j < argc)
+            if(sscanf(argv[j], "%49s", a) != 1) {
+              fprintf(stderr,"Bledny argument dla -a\n");
+              return 1;
+            }
+    }
+    if (strcmp(argv[j], "-w") == 0) { //otworzenie pliku z wynikiem
+        j++;
+        if (j < argc){
+            out = fopen(argv[j], "a");
+          if (!out) {
+            fprintf(stderr, "Nie mozna otworzyc pliku");
+            return 1;
+          }
+        }
+    }
+    if (strcmp(argv[j], "-w") != 0 &&
+        strcmp(argv[j], "-a") != 0 &&
+        strcmp(argv[j], "-h") != 0 &&
+        strcmp(argv[j], "-v") != 0) {
+        fprintf(stderr,"Nieznana flaga: %s\n", argv[j]);
+        return 1;
+    }
+  }
