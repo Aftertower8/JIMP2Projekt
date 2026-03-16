08247507f5a5962bc2edeeb8ed4141698572f296
343306 <01206747@pw.edu.pl>
2026-03-16 14:44:26 +0100

    Define pkt, link, and graf structures with functions
    Added new data structures for pkt, link, and graf along with their associated functions.

--- graf.h
+++ graf.h
@@ -3,8 +3,31 @@
 
 #include "triangulacja.h"
 
-graf* wczyt_graf (graf* g);
-pkt* stw_pkt (pkt* p);
-link* stw_link (link* l);
+typedef struct pkt { 
+ double x; 
+ double op_x;
+ double y;
+ double op_y;
+ int n; 
+} pkt; 
+
+typedef struct link { 
+  int a; 
+  int b; 
+  char name;
+  double waga;
+} link; 
+
+typedef struct graf { 
+  pkt** punkty; 
+  int l_pkt;
+  link* linki;
+  int l_l;
+} graf;
+
+graf* load_graf (graf* g);
+graf* create_graf (pkt* p, int pl, link* l, int ll);
+pkt* create_pkt (double x, double op_x, double y, double op_y, int n);
+link* create_link (int a, int b, char name, double waga);
 
 #endif
