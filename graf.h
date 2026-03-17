a0b9431e919b171423173d4beeb0456484b000a4
343306 <01206747@pw.edu.pl>
2026-03-17 16:18:05 +0100

    Refactor graf.h and add new graph management functions
    Refactor graf.h to improve structure and add new functions.

--- graf.h
+++ graf.h
@@ -1,33 +1,41 @@
-#ifndef _GRAF_H_
-#define _GRAF_H_
+#ifndef _GRAF_H_ 
 
-#include "triangulacja.h"
+#define _GRAF_H_ 
 
-typedef struct pkt { 
- double x; 
- double op_x;
- double y;
- double op_y;
- int n; 
-} pkt; 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct link { 
-  int a; 
-  int b; 
-  char name;
-  double waga;
-} link; 
+typedef struct pkt {  
+    double x;  
+    double op_x; 
+    double y; 
+    double op_y; 
+    int n;  
+} pkt;  
 
-typedef struct graf { 
-  pkt** punkty; 
-  int l_pkt;
-  link* linki;
-  int l_l;
-} graf;
+typedef struct link {  
+    char name[50]; 
+    int a;  
+    int b; 
+    double waga; 
+} link;  
 
-graf* load_graf (graf* g);
-graf* create_graf (pkt* p, int pl, link* l, int ll);
-pkt* create_pkt (double x, double op_x, double y, double op_y, int n);
-link* create_link (int a, int b, char name, double waga);
+ 
 
-#endif
+typedef struct graf {  
+    pkt* punkty;  
+    int l_pkt; 
+    link* linki; 
+    int l_l; 
+
+} graf; 
+
+graf* load_graf (char* argv); 
+graf* create_graf (); 
+int add_link(graf* g, const char* name, int a, int b, double waga);
+int add_pkt(graf* g, int n);
+int find_pkt(graf* g, int n);
+void free_graf(graf* g);
+
+#endif 
