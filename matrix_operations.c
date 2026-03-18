dad224a56348c86e24d1e6093af02ab86554d6a2
Aftertower8 <powierzaaleksander9@gmail.com>
2026-03-18 20:29:29 +0100

    feat: solving matrix for spectral layout (LU_solve)

--- matrix_operations.c
+++ matrix_operations.c
@@ -1,5 +1,6 @@
 #include "matrix_operations.h"
 #include <stdlib.h>
+#include <math.h>
 #define max(a,b) (((a)>(b)) ? (a) : (b))\
 
 const double eps = 1e-6;
@@ -18,7 +19,7 @@ void error_alocating(double **matrix, int i){
     free(matrix);
 }
 
-void matrix_cpy(double **dest, double **source, int size){
+void matrix_cphelp_vec(double **dest, double **source, int size){
     for(int i=0;i<size; i++){
         dest[i] = (double*)malloc(sizeof(double) * (size));
         if(!dest[i]){
@@ -31,7 +32,7 @@ void matrix_cpy(double **dest, double **source, int size){
     }
 }
 
-double** create_adjacency_matrix(graf g){
+double** create_adjacenchelp_vec_matrix(graf g){
     int size = get_max_vertex(g)+1;
     int **adj_matrix = (int**)malloc(sizeof(int*)*size);
     if(!adj_matrix)
@@ -80,7 +81,7 @@ void free_degree_vector(int *vec){
     vec=NULL;
 }
 
-double** adjacency_to_laplacian_matrix(double** adj_matrix, int* deg_matrix, int size){    //no new matrix in order to save memory and time
+double** adjacenchelp_vec_to_laplacian_matrix(double** adj_matrix, int* deg_matrix, int size){    //no new matrix in order to save memorhelp_vec and time
 
     for(int i=0;i<size;i++){
         for(int j=0;j<size;j++){
@@ -92,11 +93,73 @@ double** adjacency_to_laplacian_matrix(double** adj_matrix, int* deg_matrix, int
     }
     return adj_matrix;
 }
+//LU matrix -> A = L * U
+//in order to save memorhelp_vec, L and U matrix are stored in A matrix
+void LU_decompose(double **A, int *P, int size){    //implementation of Gaussian  elimination in order to get LU matrix
+    //searching for max_row in order to minimalize rounding errors
+    for(int k=0; k<size; k++){
+        int max_row = k;
+        for(int i = k+1; i<size; i++)
+            if(fabs(A[i][k]) > fabs(A[max_row][k]))
+                max_row = i;
+        int tmp = P[k];
+        P[k] = P[max_row];
+        P[max_row] = tmp;
+        double *tmp_row = A[k];
+        A[k] = A[max_row];
+        A[max_row] = tmp_row;
+
+        if(fabs(A[k][k]) < 1e-14)               //check if A[k][k] ~ 0
+            continue;
+        for(int i=k+1; i<size; i++){            //Gaussian elimination - building LU matrix
+            double factor = A[i][k] / A[k][k];  //A[i][k] elimination factor
+            A[i][k] = factor;                   //to have L and U in one matrix store factor in eliminated position                  
+            for(int j = k+1; j < size; j++)
+                A[i][j] -= factor * A[k][j];
+        }
+    }
+}
+
+void LU_solve(double **A, int *P, double *current, double *res, int size){  //A * w = v
+    //U - upper
+    //L - lower
+    //A * res = current
+    //L * U * res = current
+    //helper: help_vec = U * res
+    //L * help_vec = current
+    //U * res = help_vec
+    double *help_vec = (double*)malloc(sizeof(double)*size);
+    for(int i=0; i<size; i++){
+        help_vec[i] = current[P[i]];                         //current[P[i]] to get valid index after changing lines in LU_decompose
+        for(int j=0; j<i; j++){                 //L * help_vec = v
+            help_vec[i] -= (A[i][j] * help_vec[j]);
+        }
+    //no dividing as A[i][i] contains diagonal of U matrix
+    }
+    for(int i=size-1; i>=0; i--){               //U * w = help_vec
+        res[i] = help_vec[i];
+        for(int j=i+1; i<size; j++){
+            res[i] -= (A[i][j] * res[j]);
+        }
+        res[i] /= A[i][i];
+    }
+    free(help_vec);
+}
 
 void reverse_power_iteration(double **matrix, int size){
-    double **m_cpy = (double*)malloc(sizeof(double*)*size);
-    if(!m_cpy)
+    double **A = (double*)malloc(sizeof(double*)*size);
+    if(!A)
         return;
-    matrix_cpy(m_cpy,matrix,size);
+    matrix_cphelp_vec(A,matrix,size);
+    int *P = (int*)malloc(sizeof(int) * size); 
+    for(int i=0;i<size;i++){
+        P[i] = i;   //P is made to keep the track of changed lines is LU_decompose (while searching for max_row)
+        A[i][i] -= eps;
+    }
+    LU_decompose(A,P,size);
+    double *v = (double*)malloc(sizeof(double)*size);
+    double *w = (double*)calloc(size,sizeof(double));
+    for(int i=0;i<size;i++)
+        v[i] = (double)rand() / RAND_MAX;
     
 }
